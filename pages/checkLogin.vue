5eccc282a47c761f89029e5a4552f428d2fb9e2a
eduardcostea <costeaeduardnic@gmail.com>
2023-09-28 14:25:37 +0300

    signin and signup with supabase done

---
+++ pages/checkLogin.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+
+if (user.value) {
+  console.log(user.value)
+}
+
+const signOut = async () => {
+  const { error } = await client.auth.signOut()
+  if (error) console.log(error)
+}
+
+</script>
+
+<template>
+  <div>
+    <button @click="signOut">
+      Sign Out
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+const email = ref('')
+const password = ref('')
+
+let authenticated
+
+authenticated = user.value;
+
+const signUp = async () => {
+  const { error } = await client.auth.signUp({
+    email: email.value,
+    password: password.value
+  })
+  if (error) console.log(error)
+}
+
+//sign in function for the supabase client
+const signIn = async () => {
+  const { error } = await client.auth.signInWithPassword({
+    email: email.value,
+    password: password.value
+  })
+  if (error) console.log(error)
+}
+
+//sign out function for the supabase client
+const signOut = async () => {
+  const { error } = await client.auth.signOut()
+  if (error) console.log(error)
+}
+
+</script>
+<template>
+<div v-if="!authenticated">
+    <button @click="signUp">
+      Sign up with user password
+    </button>
+    <input
+        v-model="email"
+        type="email"
+    />
+    <input
+        v-model="password"
+        type="password"
+    />
+    <button @click="signIn">
+      Sign in
+    </button>
+</div>
+  <div v-else>
+    <button @click="signOut">
+      Sign Out
+    </button>
+  </div>
+</template>
