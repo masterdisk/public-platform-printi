d831c9c65ae1a9fe492fdc157bdfcc7460f496f5
eduardcostea <costeaeduardnic@gmail.com>
2023-09-29 15:04:51 +0300

    added dashboard template and modified the route guard in middleware

--- components/tailwindUI/application-ui/application-shells/sidebar/brand_sidebar.vue
+++ components/tailwindUI/application-ui/application-shells/sidebar/brand_sidebar.vue
@@ -1,6 +1,11 @@
 <script setup>
-import { ref } from 'vue'
-import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { ref } from "vue";
+import {
+  Dialog,
+  DialogPanel,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
 import {
   Bars3Icon,
   CalendarIcon,
@@ -10,18 +15,18 @@ import {
   InboxIcon,
   UsersIcon,
   XMarkIcon,
-} from '@heroicons/vue/24/outline'
+} from "@heroicons/vue/24/outline";
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
-]
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Team", href: "#", icon: UsersIcon, current: false },
+  { name: "Projects", href: "#", icon: FolderIcon, current: false },
+  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
+  { name: "Documents", href: "#", icon: InboxIcon, current: false },
+  { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
+];
 
-const sidebarOpen = ref(false)
+const sidebarOpen = ref(false);
 </script>
 
 <template>
@@ -34,50 +39,110 @@ const sidebarOpen = ref(false)
     ```
   -->
   <div>
-    <TransitionRoot as="template" :show="sidebarOpen">
-      <Dialog as="div" class="relative z-40 md:hidden" @close="sidebarOpen = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
+    <TransitionRoot :show="sidebarOpen" as="template">
+      <Dialog
+        as="div"
+        class="relative z-40 md:hidden"
+        @close="sidebarOpen = false"
+      >
+        <TransitionChild
+          as="template"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
           <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
         </TransitionChild>
 
         <div class="fixed inset-0 z-40 flex">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-            <DialogPanel class="relative flex w-full max-w-xs flex-1 flex-col bg-indigo-700">
-              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+          <TransitionChild
+            as="template"
+            enter="transition ease-in-out duration-300 transform"
+            enter-from="-translate-x-full"
+            enter-to="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leave-from="translate-x-0"
+            leave-to="-translate-x-full"
+          >
+            <DialogPanel
+              class="relative flex w-full max-w-xs flex-1 flex-col bg-indigo-700"
+            >
+              <TransitionChild
+                as="template"
+                enter="ease-in-out duration-300"
+                enter-from="opacity-0"
+                enter-to="opacity-100"
+                leave="ease-in-out duration-300"
+                leave-from="opacity-100"
+                leave-to="opacity-0"
+              >
                 <div class="absolute top-0 right-0 -mr-12 pt-2">
-                  <button type="button" class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
+                  <button
+                    class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    type="button"
+                    @click="sidebarOpen = false"
+                  >
                     <span class="sr-only">Close sidebar</span>
-                    <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                    <XMarkIcon aria-hidden="true" class="h-6 w-6 text-white" />
                   </button>
                 </div>
               </TransitionChild>
               <div class="h-0 flex-1 overflow-y-auto pt-5 pb-4">
                 <div class="flex flex-shrink-0 items-center px-4">
-                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300" alt="Your Company">
+                  <img
+                    alt="Your Company"
+                    class="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
+                  />
                 </div>
                 <nav class="mt-5 space-y-1 px-2">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="group flex items-center px-2 py-2 text-base font-medium rounded-md" :class="[item.current ? 'bg-indigo-800 text-white' : 'text-white hover:bg-indigo-600 hover:bg-opacity-75']">
-                    <component :is="item.icon" class="mr-4 h-6 w-6 flex-shrink-0 text-indigo-300" aria-hidden="true" />
+                  <a
+                    v-for="item in navigation"
+                    :key="item.name"
+                    :class="[
+                      item.current
+                        ? 'bg-indigo-800 text-white'
+                        : 'text-white hover:bg-indigo-600 hover:bg-opacity-75',
+                    ]"
+                    :href="item.href"
+                    class="group flex items-center px-2 py-2 text-base font-medium rounded-md"
+                  >
+                    <component
+                      :is="item.icon"
+                      aria-hidden="true"
+                      class="mr-4 h-6 w-6 flex-shrink-0 text-indigo-300"
+                    />
                     {{ item.name }}
                   </a>
                 </nav>
               </div>
               <div class="flex flex-shrink-0 border-t border-indigo-800 p-4">
-                <a href="#" class="group block flex-shrink-0">
+                <a class="group block flex-shrink-0" href="#">
                   <div class="flex items-center">
                     <div>
-                      <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                      <img
+                        alt=""
+                        class="inline-block h-10 w-10 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      />
                     </div>
                     <div class="ml-3">
                       <p class="text-base font-medium text-white">Tom Cook</p>
-                      <p class="text-sm font-medium text-indigo-200 group-hover:text-white">View profile</p>
+                      <p
+                        class="text-sm font-medium text-indigo-200 group-hover:text-white"
+                      >
+                        View profile
+                      </p>
                     </div>
                   </div>
                 </a>
               </div>
             </DialogPanel>
           </TransitionChild>
-          <div class="w-14 flex-shrink-0" aria-hidden="true">
+          <div aria-hidden="true" class="w-14 flex-shrink-0">
             <!-- Force sidebar to shrink to fit close icon -->
           </div>
         </div>
@@ -90,24 +155,50 @@ const sidebarOpen = ref(false)
       <div class="flex min-h-0 flex-1 flex-col bg-indigo-700">
         <div class="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
           <div class="flex flex-shrink-0 items-center px-4">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300" alt="Your Company">
+            <img
+              alt="Your Company"
+              class="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
+            />
           </div>
           <nav class="mt-5 flex-1 space-y-1 px-2">
-            <a v-for="item in navigation" :key="item.name" :href="item.href" class="group flex items-center px-2 py-2 text-sm font-medium rounded-md" :class="[item.current ? 'bg-indigo-800 text-white' : 'text-white hover:bg-indigo-600 hover:bg-opacity-75']">
-              <component :is="item.icon" class="mr-3 h-6 w-6 flex-shrink-0 text-indigo-300" aria-hidden="true" />
+            <a
+              v-for="item in navigation"
+              :key="item.name"
+              :class="[
+                item.current
+                  ? 'bg-indigo-800 text-white'
+                  : 'text-white hover:bg-indigo-600 hover:bg-opacity-75',
+              ]"
+              :href="item.href"
+              class="group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+            >
+              <component
+                :is="item.icon"
+                aria-hidden="true"
+                class="mr-3 h-6 w-6 flex-shrink-0 text-indigo-300"
+              />
               {{ item.name }}
             </a>
           </nav>
         </div>
         <div class="flex flex-shrink-0 border-t border-indigo-800 p-4">
-          <a href="#" class="group block w-full flex-shrink-0">
+          <a class="group block w-full flex-shrink-0" href="#">
             <div class="flex items-center">
               <div>
-                <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                <img
+                  alt=""
+                  class="inline-block h-9 w-9 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                />
               </div>
               <div class="ml-3">
                 <p class="text-sm font-medium text-white">Tom Cook</p>
-                <p class="text-xs font-medium text-indigo-200 group-hover:text-white">View profile</p>
+                <p
+                  class="text-xs font-medium text-indigo-200 group-hover:text-white"
+                >
+                  View profile
+                </p>
               </div>
             </div>
           </a>
@@ -115,8 +206,14 @@ const sidebarOpen = ref(false)
       </div>
     </div>
     <div class="flex flex-1 flex-col md:pl-64">
-      <div class="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
-        <button type="button" class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="sidebarOpen = true">
+      <div
+        class="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden"
+      >
+        <button
+          type="button"
+          class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          @click="sidebarOpen = true"
+        >
           <span class="sr-only">Open sidebar</span>
           <Bars3Icon class="h-6 w-6" aria-hidden="true" />
         </button>
@@ -124,14 +221,14 @@ const sidebarOpen = ref(false)
       <main class="flex-1">
         <div class="py-6">
           <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">
-              Dashboard
-            </h1>
+            <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
           </div>
           <div class="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
             <!-- Replace with your content -->
             <div class="py-4">
-              <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+              <div
+                class="h-96 rounded-lg border-4 border-dashed border-gray-200"
+              />
             </div>
             <!-- /End replace -->
           </div>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,5 @@
 <script lang="ts" setup>
-const client = useSupabaseClient();
-
-const signOut = async () => {
-  const { error } = await client.auth.signOut();
-  if (error) console.log(error);
-  else navigateTo("/login");
-};
+import brandSidebar from "~/components/tailwindUI/application-ui/application-shells/sidebar/brand_sidebar_with_light_header.vue";
 
 definePageMeta({
   middleware: "auth",
@@ -14,8 +8,7 @@ definePageMeta({
 
 <template>
   <div>
-    <h2 class="text-blue-700">Dashboard</h2>
-    <button @click="signOut">Sign Out</button>
+    <brandSidebar />
   </div>
 </template>
 
