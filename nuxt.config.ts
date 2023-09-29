60efbfbc4f709a544d4e087a506b428bdd02f3c4
eduardcostea <costeaeduardnic@gmail.com>
2023-09-29 12:10:00 +0300

    reversed broken nuxt.config and login from error stashed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase", "tailwindcss"],
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules: ["@nuxtjs/supabase", "tailwindcss", "@pinia/nuxt"],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  pinia: {
+    autoImports: [
+      // automatically imports `defineStore`
+      "defineStore", // import { defineStore } from 'pinia'
+      ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL || "http://localhost:3000",
+    },
+  },
+  supabase: {
+    // Options
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      exclude: ["/", "/dashboard"],
+    },
+    cookieOptions: {
+      maxAge: 60 * 60 * 8,
+      sameSite: "lax",
+      secure: true,
+    },
+    clientOptions: {
+      auth: {
+        flowType: "pkce",
+        detectSessionInUrl: true,
+        persistSession: true,
+        autoRefreshToken: true,
+      },
     },
   },
 });
--- pages/login.vue
+++ pages/login.vue
@@ -1,53 +1,6 @@
 <script lang="ts" setup>
-<<<<<<< Updated upstream
-const client = useSupabaseClient();
-const user = useSupabaseUser();
-
-const email = ref("");
-const password = ref("");
-
-const authenticated = user.value;
-
-const signUp = async () => {
-  const { error } = await client.auth.signUp({
-    email: email.value,
-    password: password.value,
-  });
-  if (error) console.log(error);
-};
-
-// sign in function for the supabase client
-const signIn = async () => {
-  const { error } = await client.auth.signInWithPassword({
-    email: email.value,
-    password: password.value,
-  });
-  if (error) console.log(error);
-};
-
-// sign out function for the supabase client
-const signOut = async () => {
-  const { error } = await client.auth.signOut();
-  if (error) console.log(error);
-};
-</script>
-<template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <div v-if="!authenticated">
-      <button @click="signUp">Sign up with user password</button>
-      <input v-model="email" type="email" />
-      <input v-model="password" type="password" />
-      <button @click="signIn">Sign in</button>
-    </div>
-    <div v-else>
-      <button @click="signOut">Sign Out</button>
-    </div>
-  </div>
-=======
 import simpleLogin from "~/components/tailwindUI/application-ui/forms/sign-in-forms/simple_card.vue";
 </script>
 <template>
   <simpleLogin />
->>>>>>> Stashed changes
 </template>
